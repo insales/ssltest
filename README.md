9a3d311c12bee4ab93c0e710793fd895d368f31c
Vadim Tivikov <vadim.tivikov@insales.ru>
2021-03-01 18:02:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# ssltest
+
+Программа для тестирования сертификатов.
+
+Многопоточная, хотя и в 1 поток проверяет сертификаты довольно быстро
+
+Проверяет домены переданные как через аргумент строки, так и через stdin, на случай если доменов будет много
+```
+ssltest -d "api.insales.ru"
+OK - Good: [api.insales.ru]
+```
+
+Резолвит ip и проверяет сертификат на каждом хосте, в оригинале откуда брал были какие-то хаки для ipv6 https://github.com/wycore/check-ssl
+```
+ssltest -d "ietf.com" -debug
+2021/03/01 14:56:52 Worker 4 start with domain ietf.com
+2021/03/01 14:56:53 ietf.com (104.200.22.130) OK. remainingValidity: 1877h22m5.893788988s
+2021/03/01 14:56:53 ietf.com (104.200.23.95) OK. remainingValidity: 1877h22m5.280547167s
+2021/03/01 14:56:53 Worker 4 finish with domain ietf.com
+2021/03/01 14:56:53 okDomains: [ietf.com]
+2021/03/01 14:56:53 warningDomains: []
+2021/03/01 14:56:53 criticalDomains: []
+OK - Good: [ietf.com]
+```
+
+Детектит почти все проблемы, которые есть на badssl.com
+```
+ssltest -d "expired.badssl.com wrong.host.badssl.com self-signed.badssl.com untrusted-root.badssl.com  null.badssl.com mitm-software.badssl.com mitm.watch sha1-2016.badssl.com"       
+Critical - Problems: [mitm.watch null.badssl.com expired.badssl.com self-signed.badssl.com mitm-software.badssl.com sha1-2016.badssl.com wrong.host.badssl.com untrusted-root.badssl.com]
+```
+
+Не детектит revoked сертификаты
+```
+ssltest -d "revoked.badssl.com" -debug
+2021/03/01 14:42:47 Worker 6 start with domain revoked.badssl.com
+2021/03/01 14:42:48 revoked.badssl.com (104.154.89.105) OK. remainingValidity: 5301h17m11.290908216s
+2021/03/01 14:42:48 Worker 6 finish with domain revoked.badssl.com
+2021/03/01 14:42:48 okDomains: [revoked.badssl.com]
+2021/03/01 14:42:48 warningDomains: []
+2021/03/01 14:42:48 criticalDomains: []
+OK - Good: [revoked.badssl.com]
+```
+```
+ssltest -d "mozilla-old.badssl.com mozilla-intermediate.badssl.com mozilla-modern.badssl.com no-sct.badssl.com"
+OK - Good: [mozilla-modern.badssl.com mozilla-intermediate.badssl.com no-sct.badssl.com mozilla-old.badssl.com]
+```
+
+И выдает результат в формате пригодном для nagios
+```
+echo "github.com" | ssltest
+OK - Good: [github.com]
+
+```
+Waring если осталось меньше дней, чем задано параметром -w
+```
+echo "www.githubstatus.com" | ssltest -w 70
+Warning - Problems: [www.githubstatus.com]
+
+```
+
+Critical отдается при проблемах с сертификатом, если домен не резолвится, либо осталось дней меньше, чем задано параметром -с
+```
+echo "example" | ssltest      
+Critical - Problems: [example]
+
+```
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+    "bufio"
+    "crypto/tls"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net"
+    "os"
+    "strings"
+    "time"
+)
+
+const (
+    OK       = iota
+    Warning  = iota
+    Critical = iota
+    Unknown  = iota
+)
+
+type CheckResult struct {
+    domain string
+    status int
+}
+
+
+func main() {
+    var warningFlag, criticalFlag, workersPool uint
+    var hosts string
+    var warningValidity, criticalValidity time.Duration
+    var debug bool
+    flag.StringVar(&hosts, "d", "", "the domain names of the hosts to check, if not provided then read from stdin")
+    flag.UintVar(&warningFlag, "w", 25, "warning validity in days")
+    flag.UintVar(&criticalFlag, "c", 14, "critical validity in days")
+    flag.UintVar(&workersPool, "p", 10, "count of workers")
+    flag.BoolVar(&debug, "debug", false, "print debug information")
+    flag.Parse()
+    warningValidity = time.Duration(warningFlag) * 24 * time.Hour
+    criticalValidity = time.Duration(criticalFlag) * 24 * time.Hour
+
+    if !debug {
+        // Если debug отключен то выводим логи в /dev/null
+        // может правильнее было бы их не выводить, но тогда пришлось бы заморачиваться
+        log.SetOutput(ioutil.Discard)
+    }
+    
+    var domains = []string{}
+    if hosts == "" {
+        // Чтение списка доменов из stdin
+        hosts, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+    }
+    domains = strings.Fields(hosts)
+    numDomains := len(domains)
+    
+    // Создание пула воркеров
+    // Взято из https://gobyexample.com/worker-pools
+    jobs := make(chan string, numDomains)
+    results := make(chan CheckResult, numDomains)
+    for w := 1; w <= int(workersPool); w++ {
+        go worker(w, jobs, results, warningValidity, criticalValidity)
+    }
+    for _, domain := range domains {
+        jobs <- domain
+    }
+    close(jobs)
+
+    var okDomains []string
+    var warningDomains []string
+    var criticalDomains []string
+    for a := 1; a <= numDomains; a++ {
+        result := <-results
+        switch result.status {
+            case OK:
+                okDomains = append(okDomains, result.domain)
+            case Warning:
+                warningDomains = append(warningDomains, result.domain)
+            case Critical:
+                criticalDomains = append(criticalDomains, result.domain)
+        }
+    }
+    log.Printf("okDomains: %s", okDomains)
+    log.Printf("warningDomains: %s", warningDomains)
+    log.Printf("criticalDomains: %s", criticalDomains)
+
+    // Выводим результат в формате удобном для Nagios
+    if len(criticalDomains)>0 {
+        fmt.Printf("Critical - Problems: %s\n", criticalDomains)
+        os.Exit(Critical)
+    } else if len(warningDomains)>0 {
+        fmt.Printf("Warning - Problems: %s\n", warningDomains)
+        os.Exit(Warning)
+    } else if len(okDomains)>0 {
+        fmt.Printf("OK - Good: %s\n", okDomains)
+        os.Exit(OK)
+    }  
+}
+
+// Во многом взято из https://github.com/wycore/check-ssl/blob/master/check-ssl.go
+func worker(id int, jobs <-chan string, results chan<- CheckResult, warningValidity time.Duration, criticalValidity time.Duration) {
+    for domain := range jobs {
+        var certificateStatus = OK
+        log.Printf("Worker %d start with domain %s", id, domain)
+        // Резолвинг ip
+        ips, err := net.LookupIP(domain)
+        if err != nil {
+            certificateStatus = Critical
+            log.Printf("%s set Critical. LookupIP: %s", domain, err)
+        }
+        // Проверяем каждый ip на который резолвится домен
+        for _, ip := range ips {
+            const connectionTimeout = 30*time.Second
+            dialer := net.Dialer{Timeout: connectionTimeout, Deadline: time.Now().Add(connectionTimeout + 5*time.Second)}
+            connection, err := tls.DialWithDialer(&dialer, "tcp", fmt.Sprintf("[%s]:443", ip), &tls.Config{ServerName: domain})
+            if err != nil {
+                certificateStatus = Critical
+                log.Printf("%s (%s) set Critical. DialWithDialer: %s", domain, ip, err)
+                continue
+            }
+            checkedCerts := make(map[string]struct{})
+            for _, chain := range connection.ConnectionState().VerifiedChains {
+                for _, cert := range chain {
+                    if _, checked := checkedCerts[string(cert.Signature)]; checked {
+                        continue
+                    }
+                    checkedCerts[string(cert.Signature)] = struct{}{}
+                    if cert.IsCA {
+                        // Не проверяем Certificate Authority, если корневые сертификаты недействительны
+                        // то ошибка будет уже на DialWithDialer 
+                        continue
+                    }
+                    // Вычисляем как долго еще действителен ssl
+                    remainingValidity := cert.NotAfter.Sub(time.Now())
+                    if remainingValidity < criticalValidity {
+                        certificateStatus = Critical
+                        log.Printf("%s (%s) set Critical. remainingValidity: %s < %s", domain, ip, remainingValidity, criticalValidity)
+                    } else if remainingValidity < warningValidity && certificateStatus != Critical {
+                        certificateStatus = Warning
+                        log.Printf("%s (%s) set Warning. remainingValidity: %s < %s", domain, ip, remainingValidity, warningValidity)
+                    } else {
+                        log.Printf("%s (%s) OK. remainingValidity: %s", domain, ip, remainingValidity)
+                    }
+                }
+            }
+            connection.Close()
+        }
+
+        results <- CheckResult{domain: domain, status: certificateStatus}
+        log.Printf("Worker %d finish with domain %s", id, domain)
+    }
+}
